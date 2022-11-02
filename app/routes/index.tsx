07789d3492bbbab8c80cf52b194110b06d27291a
Bao <huynhlbg@mail.uc.edu>
2022-11-02 18:23:09 -0400

    Add basic Prisma

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,11 +1,21 @@
-import { LinksFunction } from '@remix-run/node';
+import { LinksFunction, json } from '@remix-run/node';
+import { useLoaderData, Link } from '@remix-run/react';
 import styles from '~/styles/index.css';
+import { db } from '~/utils/db.server';
 
 export const links: LinksFunction = () => {
 	return [{ rel: 'stylesheet', href: styles }];
 };
 
+export const loader = async () => {
+	const data = {
+		notes: await db.note.findMany(),
+	};
+	return json(data);
+};
+
 export default function Index() {
+	const { notes } = useLoaderData<typeof loader>();
 	return (
 		<>
 			<header className="py-4 flex flex-row justify-between items-center">
@@ -13,16 +23,24 @@ export default function Index() {
 				<nav>
 					<ul className="flex flex-row gap-12">
 						<li>
-							<a href="#">Home</a>
+							<Link to="#">Home</Link>
 						</li>
 						<li>
-							<a href="#">Login</a>
+							<Link to="#">Login</Link>
 						</li>
 					</ul>
 				</nav>
 			</header>
 			<main className="main-grid">
-				<div>Post list</div>
+				<div>
+					<ul>
+						{notes.map((note) => (
+							<li key={note.id}>
+								<Link to={note.id}>{note.title}</Link>
+							</li>
+						))}
+					</ul>
+				</div>
 				<div>Post content</div>
 			</main>
 		</>
---
+++ prisma/seed.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client';
+const db = new PrismaClient();
+
+async function seed() {
+	await Promise.all(
+		getNotes().map((note) => {
+			return db.note.create({ data: note });
+		})
+	);
+}
+
+seed();
+
+function getNotes() {
+	return [{ title: 'Hello world', content: '# Hello world' }];
+}
