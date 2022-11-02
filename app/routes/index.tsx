2452ae31a69ce40424c0894a4483a3922e30546b
Bao <huynhlbg@mail.uc.edu>
2022-11-01 20:44:12 -0400

    Basic frontend barebone

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,30 @@
+import { LinksFunction } from '@remix-run/node';
+import styles from '~/styles/index.css';
+
+export const links: LinksFunction = () => {
+	return [{ rel: 'stylesheet', href: styles }];
+};
+
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+	return (
+		<>
+			<header className="py-4 flex flex-row justify-between items-center">
+				<div>Logo</div>
+				<nav>
+					<ul className="flex flex-row gap-12">
+						<li>
+							<a href="#">Home</a>
+						</li>
+						<li>
+							<a href="#">Login</a>
+						</li>
+					</ul>
+				</nav>
+			</header>
+			<main className="main-grid">
+				<div>Post list</div>
+				<div>Post content</div>
+			</main>
+		</>
+	);
 }
