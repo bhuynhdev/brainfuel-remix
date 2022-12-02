f7bec30819a955a77e9d76d799774393eb0607b2
Bao Huynh <55563202+bhuynhdev@users.noreply.github.com>
2022-12-01 22:25:33 -0500

    Add debounced username check

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -1,7 +1,9 @@
 import { ActionArgs, json, LinksFunction } from '@remix-run/node';
 import { useFetcher, useSearchParams } from '@remix-run/react';
+import { useState, useEffect } from 'react';
 import { z } from 'zod';
 import styles from '~/styles/login-register.css';
+import { useDebounce } from '~/utils/hooks';
 
 export const links: LinksFunction = () => {
 	return [{ href: styles, rel: 'stylesheet' }];
@@ -89,29 +91,58 @@ Y88b  d88P Y88..88P 888  888  888 888 d88P Y88..88P 888  888 Y8b.     888  888 Y
 */
 
 export default function Register(): JSX.Element {
+	let [username, setUsername] = useState('');
 	const [searchParams] = useSearchParams();
-	const fetcher = useFetcher<ActionData>();
+	const registerFetcher = useFetcher<ActionData>();
+	const usernameCheckFetcher = useFetcher();
+	const [debouncedUsername] = useDebounce(username, 700);
+
+	useEffect(() => {
+		let searchParams = new URLSearchParams(location.search);
+		if (debouncedUsername !== '') {
+			searchParams.set('q', debouncedUsername);
+		} else {
+			searchParams.delete('q');
+		}
+		// Check if username is available every time debounced username changes
+		usernameCheckFetcher.submit(searchParams, { action: 'register/username', method: 'get' });
+	}, [debouncedUsername]);
+
 	return (
 		<div className="mt-20 flex h-full flex-col items-center">
 			<h1>Register</h1>
-			<fetcher.Form method="post" className="flex w-4/5 max-w-sm flex-col gap-3">
+			<pre>{JSON.stringify(usernameCheckFetcher.data, null, 2)}</pre>
+			<registerFetcher.Form method="post" className="flex w-4/5 max-w-sm flex-col gap-3">
 				<input type="hidden" name="redirectTo" value={searchParams.get('redirectTo') ?? undefined} />
 				<div className="form-field">
 					<label htmlFor="register-username">Username</label>
-					<input type="text" id="register-username" name="username" required />
+					<input
+						type="text"
+						id="register-username"
+						name="username"
+						required
+						value={username}
+						onChange={(e) => {
+							setUsername(e.target.value);
+						}}
+					/>
 				</div>
-				{fetcher.data?.fieldErrors?.username ? (
+				{registerFetcher.data?.fieldErrors?.username ? (
 					<p className="form-validation-error" role="alert" id="username-error">
-						{fetcher.data.fieldErrors.username}
+						{registerFetcher.data.fieldErrors.username}
+					</p>
+				) : usernameCheckFetcher.data?.isUsernameUnavailable ? (
+					<p className="form-validation-error" role="alert" id="password-error">
+						Username is already used. Please choose a new username
 					</p>
 				) : null}
 				<div className="form-field">
 					<label htmlFor="register-username">Password</label>
 					<input type="password" id="register-password" name="password" required autoComplete="new-password" />
 				</div>
-				{fetcher.data?.fieldErrors?.password ? (
+				{registerFetcher.data?.fieldErrors?.password ? (
 					<p className="form-validation-error" role="alert" id="password-error">
-						{fetcher.data?.fieldErrors?.password}
+						{registerFetcher.data?.fieldErrors?.password}
 					</p>
 				) : null}
 				<div className="form-field">
@@ -124,15 +155,15 @@ export default function Register(): JSX.Element {
 						autoComplete="new-password"
 					/>
 				</div>
-				{fetcher.data?.fieldErrors?.passwordConfirm ? (
+				{registerFetcher.data?.fieldErrors?.passwordConfirm ? (
 					<p className="form-validation-error" role="alert" id="password-error">
-						{fetcher.data?.fieldErrors?.passwordConfirm}
+						{registerFetcher.data?.fieldErrors?.passwordConfirm}
 					</p>
 				) : null}
 				<button type="submit" className="rounded-lg bg-blue-400 px-4 py-2">
 					Register
 				</button>
-			</fetcher.Form>
+			</registerFetcher.Form>
 		</div>
 	);
 }
---
+++ app/routes/register.username.tsx
@@ -0,0 +1,13 @@
+import { json, LoaderArgs } from '@remix-run/node';
+import { db } from '~/utils/db.server';
+
+export async function loader({ request }: LoaderArgs) {
+	const url = new URL(request.url);
+	const query = url.searchParams.get('q');
+	console.log('query', query);
+	if (!query) {
+		return json({ isGoodUsername: true });
+	}
+	const isUsernameAvailable = (await db.user.count({ where: { username: query } })) === 0;
+	return json({ isUsernameUnavailable: !isUsernameAvailable });
+}
