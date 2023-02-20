2bc7fcaa3b65494ba2fb6de48280c306128917f1
Bao Huynh <55563202+bhuynhdev@users.noreply.github.com>
2023-02-20 01:50:06 -0500

    Conditionally invoke CodeQuiz using data.meta field

---
+++ app/components/CodeQuiz.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface CodeQuizProps {
+	prop?: string;
+}
+
+const CodeQuiz = ({ prop }: CodeQuizProps): JSX.Element => {
+	return <div>CodeQuiz</div>;
+};
+
+export default CodeQuiz;
--- app/components/MarkdownRenderer.tsx
+++ app/components/MarkdownRenderer.tsx
@@ -1,19 +1,27 @@
 import React, { memo } from 'react';
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown, { type Components as ReactMarkdownComponents } from 'react-markdown';
 import rehypeHighlight from 'rehype-highlight';
 import remarkDirective from 'remark-directive';
 import remarkDirectiveRehype from 'remark-directive-rehype';
+import CodeQuiz from './CodeQuiz';
 
 interface MarkdownRendererProps {
 	content: string;
 }
 
+const componentsOptions: ReactMarkdownComponents = {
+	// The "inline" prop isn't part of normal HTML attributes and causes issue of "received 'true' for non-boolean attribute"
+	// Thus take it out before spreading the HTMl props
+	code: ({ node, children, inline, ...props }) =>
+		(node.data?.meta as string)?.includes('quiz') ? <CodeQuiz /> : <code {...props}>{children}</code>,
+};
+
 const MarkdownRenderer: React.FC<MarkdownRendererProps> = memo(({ content }) => {
 	return (
 		<ReactMarkdown
 			remarkPlugins={[remarkDirective, remarkDirectiveRehype]}
-			remarkRehypeOptions={{ passThrough: ['quiz'] }}
 			rehypePlugins={[[rehypeHighlight, { ignoreMissing: true }]]}
+			components={componentsOptions}
 		>
 			{content}
 		</ReactMarkdown>
