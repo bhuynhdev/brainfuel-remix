3aa4eebfbfa0df5cfa30764f4846bcbc14ae2649
Bao Huynh <55563202+bhuynhdev@users.noreply.github.com>
2023-03-11 23:01:37 -0500

    Better seed file & migration to init db schema

---
+++ prisma/migrations/20230312003259_init_db_schema/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE `Note` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `title` VARCHAR(191) NOT NULL DEFAULT 'Untitled',
+    `content` TEXT NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+
+    INDEX `Note_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `passwordHash` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `User_username_key`(`username`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,23 +1,64 @@
 import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
 const db = new PrismaClient();
 
 async function seed() {
-	const bao = await db.user.create({
-		data: {
-			username: 'bao',
-			// this is a hashed version of "twixrox"
-			passwordHash: '$2b$10$K7L1OJ45/4Y2nIvhRVpCe.FSmhDdWoXehVzJptJ/op0lSsvqNu/1u',
-		},
+	const passwordHash = await bcrypt.hash('password', 10);
+	const devUser = await db.user.create({
+		data: { username: 'demo', passwordHash },
 	});
 	await Promise.all(
 		getNotes().map((note) => {
-			return db.note.create({ data: { ...note, userId: bao.id } });
+			return db.note.create({ data: { ...note, userId: devUser.id } });
 		})
 	);
 }
 
-seed();
+seed()
+	.then(async () => {
+		await db.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await db.$disconnect();
+		process.exit(1);
+	});
 
 function getNotes() {
-	return [{ title: 'Hello world', content: '# Hello world' }];
+	return [{ title: 'Using BrainFuel🧠', content: NOTE_SEED_CONTENT }];
 }
+
+const NOTE_SEED_CONTENT = ` # This is markdown
+
+## Quickly create heading 2
+
+Bold text with **double asterisks**; italic with *single asterisk*
+
+> Beautiful quote with ">"  
+People who are crazy enough to think they can change the world, are the ones who do. - Steve Jobs
+
+Create a fenced code block
+
+\`\`\`python
+a = 3
+print("Hello")
+\`\`\`
+
+Make it a quiz by just adding \`quiz\`
+\`\`\`python quiz
+a = 3
+print("Hello")
+\`\`\`
+
+For even more fun, add some flashcards to make learning more interactive ([higher-order learning](https://www.proquest.com/docview/2512912872/fulltextPDF/95CC14651BC14204PQ/1?accountid=2909))
+
+Start a question with \`?>\` mark, and designate answers with \`>\`
+
+\`\`\`qa
+?> What is circumference of a circle?
+> 2*pi*R
+
+?> What is area of a circle?
+> pi*R^2
+\`\`\`
+`;
